70486e5903eb30c6d77cc1c61c4cb80dfb95f840
Ademola309 <oyajimiademola@gmail.com>
2024-11-21 03:14:21 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const myfavDay = 'Javascript'
+
+const day = 'Sunday'
+
+const position = 1
+
+const output = 'Today is ' + day + '.' +  ' It is the ' + position + 'day of the week.'
+
+console.log(output)
